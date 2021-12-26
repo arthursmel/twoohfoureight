d0b4805b306277eed46ceb6175045adf8c26ae78
arthursmel <arthurme@tcd.ie>
2021-12-26 17:40:15 +0000

    fix bug for invalid move

--- classes/game.py
+++ classes/game.py
@@ -2,7 +2,7 @@ from typing import Any, List, Tuple
 from typing import Optional
 from typing import Set
 from .state import State
-from .types import Row, Cells, XY
+from .types import Row, Cells, XY, Pair
 import random
 import itertools
 
@@ -30,18 +30,16 @@ class Game:
         to_add_xy = lambda x,y: (x,y) if cells[y][x] != 0 else (-1,-1)
         return set(filter(zeros, { to_add_xy(x,y) for y in range(dim) for x in range(dim) }))
          
-    def place_random_val(self, state: State) -> State:
-        used_xys = self.get_used_xys(state.cells)
+    def place_random_val(self, cells: Cells) -> Cells:
+        used_xys = self.get_used_xys(cells)
 
-        dim = len(state.cells[0])
+        dim = len(cells[0])
         xy = self.get_rand_xy(dim, excludes=used_xys)
         
-        if xy == (-1,-1): return state
+        if xy == (-1,-1): return cells
 
-        set_val = lambda x, y: self.get_next_rand_val() if (x,y) == xy else state.cells[y][x] 
-        cells = tuple(tuple(set_val(x,y) for x in range(dim)) for y in range(dim))
-
-        return State(cells, has_moves=self.has_moves(cells), has_2048=self.has_2048(cells))
+        set_val = lambda x, y: self.get_next_rand_val() if (x,y) == xy else cells[y][x] 
+        return tuple(tuple(set_val(x,y) for x in range(dim)) for y in range(dim))
 
     def move_row_horizontal(self, row: Row, dir: str) -> Row:
         ns = list(filter(lambda i: i > 0, row))
@@ -57,45 +55,39 @@ class Game:
 
         return summed + zeros if dir == 'l' else zeros + summed
 
-    def pairs(self, ns) -> Tuple[Tuple[int]]:
+    def pairs(self, ns) -> Tuple[Pair]:
         return tuple( (ns[i],ns[i+1]) if i+1 < len(ns) else (ns[i],) for i in range(0,len(ns),2) )
 
-    def offset_pairs(self, ns: Row) -> Tuple[Tuple[int]]:
+    def offset_pairs(self, ns: Row) -> Tuple[Pair]:
         if len(ns) == 0: return ()
         return ((ns[0],),) + tuple( (ns[i],ns[i+1]) if i+1 < len(ns) else (ns[i],) for i in range(1,len(ns),2) )
 
-    def pair_sum(self, ns: Row) -> Tuple[Tuple[int]]:
+    def pair_sum(self, ns: Row) -> Tuple[Pair]:
         return tuple( (n[0]+n[1],) if len(n)==2 and n[0]==n[1] else n for n in ns )
 
     def rotate_cells(self, cells: Cells, dir: str) -> Cells:
         zipped_cells = tuple(zip(*cells[::-1])) if dir == 'l' else tuple(zip(*cells))[::-1]
         return tuple(tuple(row) for row in zipped_cells)
 
-    def move_cells_horizontal(self, cells: Cells, dir: str) -> Cells:
-        return tuple(map(lambda r: self.move_row_horizontal(r, dir), cells))
+    def move_cells_horizontal(self, cells: Cells, dir: str) -> Tuple[Cells, bool]:
+        new_cells = tuple(map(lambda r: self.move_row_horizontal(r, dir), cells))
+        return (new_cells, new_cells != cells)
 
-    def move_cells_vertical(self, cells: Cells, dir: str) -> Cells:
+    def move_cells_vertical(self, cells: Cells, dir: str) -> Tuple[Cells, bool]:
         rotated_cells = self.rotate_cells(cells, 'l')
         horizontal_dir = 'l' if dir == 'd' else 'r'
-        cells = self.move_cells_horizontal(rotated_cells, horizontal_dir)
-
-        return self.rotate_cells(cells, 'r')
-
-    def move_horizontal(self, state: State, dir: str) -> State:
-        cells = self.move_cells_horizontal(state.cells, dir)
-        return State(cells, has_2048=self.has_2048(cells), has_moves=self.has_moves(cells))
-
-    def move_vertical(self, state: State, dir: str) -> State:
-        cells = self.move_cells_vertical(state.cells, dir)
-        return State(cells, has_2048=self.has_2048(cells), has_moves=self.has_moves(cells))
-
+        rotated_new_cells, _ = self.move_cells_horizontal(rotated_cells, horizontal_dir)
+        new_cells = self.rotate_cells(rotated_new_cells, 'r')
+        return (new_cells, new_cells != cells)
+        
     def move(self, state: State, dir: str) -> State:
         if dir in ('u', 'd'):
-            moved = self.move_vertical(state, dir)
-            return self.place_random_val(moved)
+            moved_cells, was_valid = self.move_cells_vertical(state.cells, dir)
         else:
-            moved = self.move_horizontal(state, dir)
-            return self.place_random_val(moved)
+            moved_cells, was_valid = self.move_cells_horizontal(state.cells, dir)
+
+        cells = self.place_random_val(moved_cells) if was_valid else moved_cells
+        return State(cells, has_2048=self.has_2048(cells), has_moves=self.has_moves(cells))
         
     def move_up(self, state: State) -> State:
         return self.move(state, dir='u')
--- classes/types.py
+++ classes/types.py
@@ -3,3 +3,4 @@ from typing import Tuple, NewType
 Row = NewType("Row", Tuple[int])
 Cells = NewType("Cells", Tuple[Row])
 XY = NewType("XY", Tuple[int])
+Pair = NewType("Pair", Tuple[int])
