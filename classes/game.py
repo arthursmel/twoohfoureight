3bdb1ee0c0e013c19d963762d28d953832fd786a
arthursmel <arthurme@tcd.ie>
2021-12-27 14:42:56 +0000

    fix bug with game

--- classes/game.py
+++ classes/game.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from typing import Any, List, Tuple
 from typing import Optional
 from typing import Set
@@ -5,6 +6,10 @@ from .state import State
 from .types import Row, Cells, XY, Pair
 import random
 import itertools
+import functools
+
+SumResult = namedtuple("SumResult", ["row", "score"])
+MoveResult = namedtuple("MoveResult", ["cells", "score", "valid"])
 
 class Game:
     def init_state(self, dim: int=4) -> State:
@@ -41,53 +46,58 @@ class Game:
         set_val = lambda x, y: self.get_next_rand_val() if (x,y) == xy else cells[y][x] 
         return tuple(tuple(set_val(x,y) for x in range(dim)) for y in range(dim))
 
-    def move_row_horizontal(self, row: Row, dir: str) -> Row:
-        ns = list(filter(lambda i: i > 0, row))
+    def move_row_horizontal(self, row: Row, dir: str) -> SumResult:
+        ns = tuple(filter(lambda i: i > 0, row))
 
-        ps = self.pairs(ns)
-        offset_ps = self.offset_pairs(ns) 
-
-        pair_sums = tuple(itertools.chain.from_iterable(self.pair_sum(ps)))
-        offset_pair_sums = tuple(itertools.chain.from_iterable(self.pair_sum(offset_ps)))
-
-        summed = min([pair_sums, offset_pair_sums], key=len)
+        summed, score = self.get_pair_sums(ns if dir == 'l' else ns[::-1])
         zeros = tuple(0 for _ in range(len(row) - len(summed)))
 
-        return summed + zeros if dir == 'l' else zeros + summed
+        return SumResult(summed + zeros if dir == 'l' else zeros + summed[::-1], score)
 
-    def pairs(self, ns) -> Tuple[Pair]:
-        return tuple( (ns[i],ns[i+1]) if i+1 < len(ns) else (ns[i],) for i in range(0,len(ns),2) )
+    def get_pair_sums(self, ns: Row) -> SumResult:
+        if len(ns) in (0,1): return SumResult(ns, 0)
+        fst, snd = ns[0], ns[1]
 
-    def offset_pairs(self, ns: Row) -> Tuple[Pair]:
-        if len(ns) == 0: return ()
-        return ((ns[0],),) + tuple( (ns[i],ns[i+1]) if i+1 < len(ns) else (ns[i],) for i in range(1,len(ns),2) )
-
-    def pair_sum(self, ns: Row) -> Tuple[Pair]:
-        return tuple( (n[0]+n[1],) if len(n)==2 and n[0]==n[1] else n for n in ns )
+        if fst == snd:
+            next_pair_sums, score = self.get_pair_sums(ns[2:])
+            pair_sums = SumResult((fst + snd,) + next_pair_sums, score + fst+snd)
+        else:
+            next_pair_sums, score = self.get_pair_sums(ns[1:])
+            pair_sums = SumResult((fst,) + next_pair_sums, score)
+        return pair_sums
 
     def rotate_cells(self, cells: Cells, dir: str) -> Cells:
-        zipped_cells = tuple(zip(*cells[::-1])) if dir == 'l' else tuple(zip(*cells))[::-1]
+        zipped_cells = tuple(zip(*cells))[::-1] if dir == 'l' else tuple(zip(*cells[::-1]))
         return tuple(tuple(row) for row in zipped_cells)
 
-    def move_cells_horizontal(self, cells: Cells, dir: str) -> Tuple[Cells, bool]:
-        new_cells = tuple(map(lambda r: self.move_row_horizontal(r, dir), cells))
-        return (new_cells, new_cells != cells)
+    def move_cells_horizontal(self, cells: Cells, dir: str) -> MoveResult:
+        sum_results = tuple(map(lambda r: self.move_row_horizontal(r, dir), cells))
 
-    def move_cells_vertical(self, cells: Cells, dir: str) -> Tuple[Cells, bool]:
+        score = sum(tuple(map(lambda r: r.score, sum_results)))
+        new_cells = tuple(map(lambda r: r.row, sum_results))
+
+        return MoveResult(new_cells, score, new_cells != cells)
+
+    def move_cells_vertical(self, cells: Cells, dir: str) -> MoveResult:
         rotated_cells = self.rotate_cells(cells, 'l')
-        horizontal_dir = 'l' if dir == 'd' else 'r'
-        rotated_new_cells, _ = self.move_cells_horizontal(rotated_cells, horizontal_dir)
+        print(rotated_cells)
+        horizontal_dir = 'r' if dir == 'd' else 'l'
+
+        rotated_new_cells, score, _ = self.move_cells_horizontal(rotated_cells, horizontal_dir)
+
+        print(rotated_new_cells)
         new_cells = self.rotate_cells(rotated_new_cells, 'r')
-        return (new_cells, new_cells != cells)
+
+        return MoveResult(new_cells, score, new_cells != cells)
         
     def move(self, state: State, dir: str) -> State:
         if dir in ('u', 'd'):
-            moved_cells, was_valid = self.move_cells_vertical(state.cells, dir)
+            new_cells, score, valid = self.move_cells_vertical(state.cells, dir)
         else:
-            moved_cells, was_valid = self.move_cells_horizontal(state.cells, dir)
+            new_cells, score, valid = self.move_cells_horizontal(state.cells, dir)
 
-        cells = self.place_random_val(moved_cells) if was_valid else moved_cells
-        return State(cells, has_2048=self.has_2048(cells), has_moves=self.has_moves(cells))
+        new_cells = self.place_random_val(new_cells) if valid else new_cells
+        return State(new_cells, has_2048=self.has_2048(new_cells), has_moves=self.has_moves(new_cells))
         
     def move_up(self, state: State) -> State:
         return self.move(state, dir='u')
